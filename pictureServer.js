26741a758a28ac9a838ed4b59430ff32cb5f972b
jonleecutler <31291496+jonleecutler@users.noreply.github.com>
2018-02-12 17:05:40 -0500

    Final css tweaks.

--- pictureServer.js
+++ pictureServer.js
@@ -80,8 +80,8 @@ var ansi_up_default = new ansi_up.default;
 var asciifyOptions = { //These Options define how the asciify library is operated.
   //Picture related
   fit: 'box',
-  width: 42,
-  height: 42
+  width: 41,
+  height: 41
 }
 
 //----------------------------------------------------------------------------//
